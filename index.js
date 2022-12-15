ef9b2ef6ce0371ca0876728834d97cacdc7ef023
jdawsonimgix <jdawson@imgix.com>
2022-12-15 12:56:26 -0800

    updated searching function and removed old code

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/test", (req, res) => {
-  res.send("test!");
-});
-
 app.post("/searching", async (req, res) => {
   console.log("Checking imgix status in /testsearch");
   let arrSearchImgix = [];
@@ -25,36 +21,9 @@ app.post("/searching", async (req, res) => {
 
   var config = {
     method: "get",
-    url: "https://api.imgix.com/api/v1/assets/622f76522d67dbae5fb46268?filter[tags]=cup",
-    headers: {
-      Authorization: "Bearer " + process.env.IMGIX_API,
-      "Content-Type": "application/vnd.api+json",
-    },
-  };
-
-  let grabAll = await axios(config)
-    .then(function (response) {
-      for (var i = 0; i < response.data.data.length; i++) {
-        arrSearchImgix.push(response.data.data[i].attributes.origin_path);
-      }
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-      return error;
-    });
-
-  return res.status(200).send(arrSearchImgix);
-});
-//
-app.post("/testsearching", async (req, res) => {
-  console.log("Checking imgix status in /testsearch");
-  let arrSearchImgix = [];
-  let sentFromFrontend = req.body.sendToBackend;
-
-  var config = {
-    method: "get",
-    url: "https://api.imgix.com/api/v1/assets/622f76522d67dbae5fb46268?filter[tags]=cup",
+    url:
+      "https://api.imgix.com/api/v1/assets/622f76522d67dbae5fb46268?filter[tags]=" +
+      sentFromFrontend,
     headers: {
       Authorization: "Bearer " + process.env.IMGIX_API,
       "Content-Type": "application/vnd.api+json",
