23c44680601d7e06f57a05f2e08ab9fcfd26467c
jdawsonimgix <jdawson@imgix.com>
2022-12-15 09:58:55 -0800

    updating functions

--- index.js
+++ index.js
@@ -49,6 +49,37 @@ app.post("/searching", async (req, res) => {
   return res.status(200).send(arrSearchImgix);
 });
 
+app.post("/testsearching", async (req, res) => {
+  console.log("Checking imgix status in /testsearch");
+  let arrSearchImgix = [];
+  let sentFromFrontend = req.body.sendToBackend;
+
+  var config = {
+    method: "get",
+    url:
+      "https://api.imgix.com/api/v1/assets/622f76522d67dbae5fb46268?filter[tags]=" +
+      sentFromFrontend,
+    headers: {
+      Authorization: "Bearer " + process.env.IMGIX_API,
+      "Content-Type": "application/vnd.api+json",
+    },
+  };
+
+  let grabAll = await axios(config)
+    .then(function (response) {
+      for (var i = 0; i < response.data.data.length; i++) {
+        arrSearchImgix.push(response.data.data[i].attributes.origin_path);
+      }
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    });
+
+  return res.status(200).send(arrSearchImgix);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
