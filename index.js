8b129f7577d4101f160c2945a086a253108c1854
jdawsonimgix <jdawson@imgix.com>
2022-12-15 09:42:06 -0800

    tadded search function to backend

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+const port = 5001;
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -8,6 +16,37 @@ app.get("/", (req, res) => {
 
 app.get("/test", (req, res) => {
   res.send("test!");
+});
+
+app.post("/searching", async (req, res) => {
+  console.log("Checking imgix status in /testsearch");
+  let arrSearchImgix = [];
+  let sentFromFrontend = req.body.sendToBackend;
+
+  var config = {
+    method: "get",
+    url:
+      "https://api.imgix.com/api/v1/assets/622f76522d67dbae5fb46268?filter[tags]=" +
+      sentFromFrontend,
+    headers: {
+      Authorization: "Bearer " + process.env.IMGIX_API,
+      "Content-Type": "application/vnd.api+json",
+    },
+  };
+
+  let grabAll = await axios(config)
+    .then(function (response) {
+      for (var i = 0; i < response.data.data.length; i++) {
+        arrSearchImgix.push(response.data.data[i].attributes.origin_path);
+      }
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    });
+
+  return res.status(200).send(arrSearchImgix);
 });
 
 app.listen(port, () => {
