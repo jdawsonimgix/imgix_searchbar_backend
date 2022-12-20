346a52294a8edf23ce565d2dcc93b0b55d3af3c7
jdawsonimgix <jdawson@imgix.com>
2022-12-19 17:55:06 -0800

    updated search functionality to search for tags, categories, and file names

--- index.js
+++ index.js
@@ -45,6 +45,42 @@ app.post("/searching", async (req, res) => {
   return res.status(200).send(arrSearchImgix);
 });
 
+app.post("/searchingall", async (req, res) => {
+  console.log("Checking imgix status in /testsearch");
+  let arrSearchImgix = [];
+  let sentFromFrontend = req.body.sendToBackend;
+
+  var config = {
+    method: "get",
+    url:
+      "https://api.imgix.com/api/v1/assets/622f76522d67dbae5fb46268?filter[or:categories]=" +
+      sentFromFrontend +
+      "&filter[or:keywords]=" +
+      sentFromFrontend +
+      "&filter[or:origin_path]=" +
+      sentFromFrontend +
+      "&page[cursor]=0&page[limit]=60&sort=-date_created",
+    headers: {
+      Authorization: "Bearer " + process.env.IMGIX_API,
+      "Content-Type": "application/vnd.api+json",
+    },
+  };
+
+  let grabAll = await axios(config)
+    .then(function (response) {
+      for (var i = 0; i < response.data.data.length; i++) {
+        arrSearchImgix.push(response.data.data[i].attributes.origin_path);
+      }
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    });
+
+  return res.status(200).send(arrSearchImgix);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
