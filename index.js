bfc678523b04436bc54fd5bfd6794dffbb91c59c
jdawsonimgix <jdawson@imgix.com>
2022-12-21 14:20:43 -0800

    removed old call function from backend

--- index.js
+++ index.js
@@ -14,37 +14,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/searching", async (req, res) => {
-  console.log("Checking imgix status in /testsearch");
-  let arrSearchImgix = [];
-  let sentFromFrontend = req.body.sendToBackend;
-
-  var config = {
-    method: "get",
-    url:
-      "https://api.imgix.com/api/v1/assets/622f76522d67dbae5fb46268?filter[tags]=" +
-      sentFromFrontend,
-    headers: {
-      Authorization: "Bearer " + process.env.IMGIX_API,
-      "Content-Type": "application/vnd.api+json",
-    },
-  };
-
-  let grabAll = await axios(config)
-    .then(function (response) {
-      for (var i = 0; i < response.data.data.length; i++) {
-        arrSearchImgix.push(response.data.data[i].attributes.origin_path);
-      }
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-      return error;
-    });
-
-  return res.status(200).send(arrSearchImgix);
-});
-
 app.post("/searchingall", async (req, res) => {
   console.log("Checking imgix status in /testsearch");
   let arrSearchImgix = [];
